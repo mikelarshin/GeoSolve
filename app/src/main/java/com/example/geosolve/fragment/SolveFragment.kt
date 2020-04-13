9327a3db24362e718aaade12b89c2e3052cde197
Michail <mlarshin@ya.ru>
2020-04-13 16:48:46 +0300

    hide FAB with scroll

--- app/src/main/java/com/example/geosolve/fragment/SolveFragment.kt
+++ app/src/main/java/com/example/geosolve/fragment/SolveFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.geosolve.MainActivity
 import com.example.geosolve.R
 import com.example.geosolve.StepSlove
 import com.example.geosolve.view.RecycleAdapter
+import kotlinx.android.synthetic.main.solve_fragment.*
 import kotlinx.android.synthetic.main.solve_fragment.view.*
+import kotlinx.android.synthetic.main.solve_fragment.view.recycler
 
 class SolveFragment : Fragment() {
 
@@ -28,6 +31,20 @@ class SolveFragment : Fragment() {
         view.recycler.setHasFixedSize(true)
 
         view.recycler.adapter = RecycleAdapter(view.context)
+
+        view.recycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0 || dy < 0 && view.back_button.isShown)
+                    view.back_button.hide()
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE)
+                    view.back_button.show();
+
+                super.onScrollStateChanged(recyclerView, newState)
+            }
+        })
 
         for (i in 1..100)
             RecycleAdapter.addItem(
