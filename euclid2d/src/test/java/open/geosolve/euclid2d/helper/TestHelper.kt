10e0adcecfb68151a0d56472d006d26321a68c4c
Klachkov Valery <0gdump.kv@gmail.com>
2020-05-25 22:44:29 +1000

    [euclid2d\test] Implement simple test helper

---
+++ euclid2d/src/test/java/open/geosolve/euclid2d/helper/TestHelper.kt
@@ -0,0 +1,16 @@
+package open.geosolve.euclid2d.helper
+
+object TestHelper {
+
+    fun test(string: String) {
+        println("[TEST] $string")
+    }
+
+    fun description(string: String) {
+        println("[DESC] $string")
+    }
+
+    fun ok(string: String) {
+        println("[ OK ] $string")
+    }
+}
