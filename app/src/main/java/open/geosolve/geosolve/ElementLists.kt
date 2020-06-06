d5febb3c8f05aa740500d24ab09f1b1313101f04
mikelarshin <mlarshin@ya.ru>
2020-06-06 13:38:22 +0300

    some fix

--- app/src/main/java/open/geosolve/geosolve/ElementLists.kt
+++ app/src/main/java/open/geosolve/geosolve/ElementLists.kt
@@ -8,57 +8,79 @@ import open.geosolve.geosolve.model.data.Node
 import org.w3c.dom.Element
 
 val AllNodes
-    get() = object : ArrayList<Node>() {
-        init {
-            this.addAll(figureList.flatMap { it.mNodes })
-        }
-
-        override fun remove(node: Node): Boolean {
-            for (figure in figureList)
-                if (node in figure.mNodes)
-                    figure.mNodes.remove(node)
-            return true
-        }
-    }
-
+    get() = NodeList.update()
 val AllLines
-    get() = object : ArrayList<Line>() {
-        init {
-            this.addAll(figureList.flatMap { it.mLines })
-        }
-
-        override fun remove(line: Line): Boolean {
-            for (figure in figureList)
-                if (line in figure.mLines)
-                    figure.mLines.remove(line)
-            return true
-        }
-    }
-
+    get() = LineList.update()
 val AllAngles
-    get() = object : ArrayList<Angle>() {
-        init {
-            this.addAll(figureList.flatMap { it.mAngles })
-        }
+    get() = AngleList.update()
+val AllCircles: CircleList
+    get() = CircleList.update()
 
-        override fun remove(angle: Angle): Boolean {
-            for (figure in figureList)
-                if (angle in figure.mAngles)
-                    figure.mAngles.remove(angle)
-            return true
-        }
+
+interface ElementList {
+    fun update(): ElementList
+}
+
+object NodeList : ArrayList<Node>(), ElementList {
+
+    override fun update(): NodeList {
+        this.clear()
+        this.addAll(figureList.flatMap { it.mNodes })
+        return this
     }
 
-val AllCircles
-    get() = object : ArrayList<Circle>() {
-        init {
-            this.addAll(figureList.flatMap { listOf(it.mCircle) }.filterNotNull())
-        }
-
-        override fun remove(circle: Circle): Boolean {
-            for (figure in figureList)
-                if (circle == figure.mCircle)
-                    figure.mCircle = null
-            return true
-        }
+    override fun remove(node: Node): Boolean {
+        for (figure in figureList)
+            if (node in figure.mNodes)
+                figure.mNodes.remove(node)
+        return true
     }
+}
+
+object LineList : ArrayList<Line>(), ElementList {
+
+    override fun update(): LineList {
+        this.clear()
+        this.addAll(figureList.flatMap { it.mLines })
+        return this
+    }
+
+    override fun remove(line: Line): Boolean {
+        for (figure in figureList)
+            if (line in figure.mLines)
+                figure.mLines.remove(line)
+        return true
+    }
+}
+
+object AngleList : ArrayList<Angle>(), ElementList {
+
+    override fun update(): AngleList {
+        this.clear()
+        this.addAll(figureList.flatMap { it.mAngles })
+        return this
+    }
+
+    override fun remove(angle: Angle): Boolean {
+        for (figure in figureList)
+            if (angle in figure.mAngles)
+                figure.mAngles.remove(angle)
+        return true
+    }
+}
+
+object CircleList : ArrayList<Circle>(), ElementList {
+
+    override fun update(): CircleList {
+        this.clear()
+        this.addAll(figureList.flatMap { listOf(it.mCircle) }.filterNotNull())
+        return this
+    }
+
+    override fun remove(circle: Circle): Boolean {
+        for (figure in figureList)
+            if (circle == figure.mCircle)
+                figure.mCircle = null
+        return true
+    }
+}
