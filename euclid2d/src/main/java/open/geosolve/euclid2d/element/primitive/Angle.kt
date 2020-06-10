08caa2805ed82952600b2f493f6255883ff30cf9
Klachkov Valery <0gdump.kv@gmail.com>
2020-06-10 23:15:44 +1000

    [euclid2d\primitive] Rework Angle

--- euclid2d/src/main/java/open/geosolve/euclid2d/element/primitive/Angle.kt
+++ euclid2d/src/main/java/open/geosolve/euclid2d/element/primitive/Angle.kt
@@ -3,10 +3,30 @@ package open.geosolve.euclid2d.element.primitive
 import open.geosolve.euclid2d.element.primitive.metric.AngleValue
 import open.geosolve.euclid2d.element.primitive.metric.Degree
 
-data class Angle(
-    val first: Point,
-    val center: Point,
-    val second: Point,
-    var value: AngleValue = Degree(),
-    var name: String = "∠${first.name}${center.name}${second.name} { $value }"
-) : PrimitiveElement()
+// TODO(CODE) Optional calculate value by points
+class Angle(first: Line, second: Line, value: AngleValue? = null) : PrimitiveElement() {
+
+    private var pseudoValue: AngleValue? = value
+    var value: AngleValue
+        get() = pseudoValue ?: Degree()  // FIXME(CODE) Calculate value by points
+        set(value) {
+            pseudoValue = value
+        }
+
+    val firstLine: Line = first
+    val secondLine: Line = second
+
+    val firstPoint: Point get() = firstLine.first
+    val centerPoint: Point get() = secondLine.first
+    val secondPoint: Point get() = secondLine.second
+
+    /*constructor(first: Point, center: Point, second: Point, value: AngleValue? = null) : super() {
+        this.firstLine = Line(first, center)
+        this.secondLine = Line(second, center)
+        this.pseudoValue = value
+    }*/
+
+    override fun toString(): String {
+        return "∠${firstPoint.name}${centerPoint.name}${secondPoint.name} { $value }"
+    }
+}
